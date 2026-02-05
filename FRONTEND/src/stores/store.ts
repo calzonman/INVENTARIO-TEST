ac94a3d94841ae8f73a091c0c5e488b935f8742d
calzonman <130697325+calzonman@users.noreply.github.com>
2026-02-05 17:04:23 -0300

    store.ts api_url

--- FRONTEND/src/stores/store.ts
+++ FRONTEND/src/stores/store.ts
@@ -1,6 +1,6 @@
 import { writable, get } from 'svelte/store';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 
 // --- INTERFACES ---
 export interface Product {
